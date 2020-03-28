899b85f7fc215d729a5c37a54bd9ea15bd6a033e
MrWilllian <willian.dev10@gmail.com>
2020-03-28 12:00:22 -0300

    refactor: define Perfil model

--- perfis/models.py
+++ perfis/models.py
@@ -3,4 +3,11 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Peril(object):
+  
+  def __init__(self, nome='', email='', telefone='', nome_empresa=''):
+    self.nome = nome
+    self.email = email
+    self.telefone = telefone
+    self.nome_empresa = nome_empresa
+    
