723da5cc2f98f4abcdb492b29419b863eb64851a
MrWilllian <willian.dev10@gmail.com>
2020-03-28 14:28:43 -0300

    refactor: add views import and change route to views methods

--- perfis/urls.py
+++ perfis/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from perfis import views
 
 urlpatterns = [
-  url(r'^$', include('perfis.views.index'), name='exibir'),
-  url(r'^perfis/(?P<perfil_id>\d+)$', 'perfis.views.exibir', name='exibir')
+  url(r'^$', include(views.index), name='index'),
+  url(r'^perfis/(?P<perfil_id>\d+)$', views.exibir, name='exibir'),
+  url(r'^perfis/(?P<perfil_id>\d+)/convidar$', views.convidar, name='convidar'),
 ]
