4306cd500690821b5d6366c470a2cabf39d286cc
MrWilllian <willian.dev10@gmail.com>
2020-03-28 15:13:02 -0300

    refactor: remove include function

--- perfis/urls.py
+++ perfis/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import include, url
+from django.conf.urls import url
 from django.contrib import admin
 from perfis import views
 
 urlpatterns = [
-  url(r'^$', include(views.index), name='index'),
+  url(r'^$', views.index, name='index'),
   url(r'^perfis/(?P<perfil_id>\d+)$', views.exibir, name='exibir'),
   url(r'^perfis/(?P<perfil_id>\d+)/convidar$', views.convidar, name='convidar'),
 ]
