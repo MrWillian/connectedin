82928b338e2f60d973f7bc610102feb734e45300
MrWilllian <willian.dev10@gmail.com>
2020-03-28 20:28:34 -0300

    refactor: add perfil logado var in index method

--- perfis/views.py
+++ perfis/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render, redirect
 from perfis.models import Perfil
 
 def index(request):
-  return render(request, 'index.html', {'perfis' : Perfil.objects.all()})
+  return render(request, 'index.html', {'perfis' : Perfil.objects.all(), 'perfil_logado': get_perfil_logado(request) })
 
 def exibir(request, perfil_id):
   print 'ID do perfil recebido: %s' % (perfil_id)
