d4571864eee80c55c28d77c27452cd6b141121ee
MrWilllian <willian.dev10@gmail.com>
2020-03-28 11:59:38 -0300

    refactor: named routes

--- perfis/urls.py
+++ perfis/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-  url(r'^$', include('perfis.views.index')),
+  url(r'^$', include('perfis.views.index'), name='exibir'),
+  url(r'^perfis/(?P<perfil_id>\d+)$', 'perfis.views.exibir', name='exibir')
 ]
