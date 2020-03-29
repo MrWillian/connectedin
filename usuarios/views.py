ee37efbada59069c746f9d2e34ea0ae5124f24c9
MrWilllian <willian.dev10@gmail.com>
2020-03-29 20:56:46 -0300

    refactor: implement post method with form data

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.base import View
+from usuarios.forms import RegistrarUsuarioForm
+from django.contrib.auth.models import User
+from perfis.models import Perfil
 
 class RegistrarUsuarioView(View):
   
@@ -12,4 +15,17 @@ class RegistrarUsuarioView(View):
     return render(request, self.template_name)
 
   def post(self, request):
-    return render(request, self.template_name)
+    form = RegistrarUsuarioForm(request.POST)
+    if form.is_valid():
+      dados_form = form.data
+
+      usuario = User.objects.create_user(dados_form['nome'],dados_form['email'],dados_form['senha'])
+      perfil = Perfil(nome=dados_form['nome'],
+                      email=dados_form['email'],
+                      telefone=dados_form['telefone'],
+                      nome_empresa=dados_form['nome_empresa'],
+                      usuario=usuario)
+      perfil.save()
+      return redirect('index')
+
+    return render(request, self.template_name, {'form' : form})
