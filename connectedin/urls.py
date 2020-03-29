e3eddd40d70d64edd1294c12c9492c77d2100255
MrWilllian <willian.dev10@gmail.com>
2020-03-29 10:46:40 -0300

    refactor: add user url in urls for project

--- connectedin/urls.py
+++ connectedin/urls.py
@@ -3,5 +3,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^', include('perfis.urls'))
+    url(r'^', include('perfis.urls')),
+    url(r'^', include('usuarios.urls')),
 ]
