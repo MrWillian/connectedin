666ba5e291c2635f387d3d27170809ac49bfdc77
MrWilllian <willian.dev10@gmail.com>
2020-03-29 10:13:23 -0300

    refactor: add accept method and contacts many to many relationship

--- perfis/models.py
+++ perfis/models.py
@@ -9,6 +9,7 @@ class Perfil(models.Model):
   email = models.CharField(max_length=255, null=False)
   telefone = models.CharField(max_length=15, null=False)
   nome_empresa = models.CharField(max_length=255, null=False)
+  contatos = models.ManyToManyField('self')
 
   def convidar(self, perfil_convidado):
     Convite(solicitante=self, convidado=perfil_convidado).save()
@@ -17,3 +18,8 @@ class Convite(models.Model):
   
   solicitante = models.ForeignKey(Perfil, related_name='convites_feitos')
   convidado = models.ForeignKey(Perfil, related_name='convites_recebidos')
+
+  def aceitar(self):
+    self.convidado.contatos.add(self.solicitante)
+    self.solicitante.contatos.add(self.convidado)
+    self.delete()
