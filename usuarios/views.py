7c1fda3d614b6dfab66a134f49029576070e9ef9
MrWilllian <willian.dev10@gmail.com>
2020-03-29 10:45:35 -0300

    refactor: user app structure

--- usuarios/views.py
+++ usuarios/views.py
@@ -2,5 +2,7 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.views.generic.base import View
 
-# Create your views here.
+class RegistrarUsuarioView(View):
+  pass
