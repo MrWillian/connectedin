80af780a91ec755ccc62589a217b500f66dd8178
MrWilllian <willian.dev10@gmail.com>
2020-03-29 20:56:19 -0300

    refactor: comment urls

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,6 +1,9 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from views import RegistrarUsuarioView
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-  url(r'^registrar/$', RegistrarUsuarioView.as_view(), name="registrar"), 
+  url(r'^registrar/$', RegistrarUsuarioView.as_view(), name="registrar"),
+  # url(r'^login/$', auth_views.LoginView.as_view(), { 'template_name' : 'login.html'}, name="login"),
+  # url(r'^logout/$', auth_views.LogoutView.as_view(), { 'login_url' : '/login'}, name="logout")
 ]
